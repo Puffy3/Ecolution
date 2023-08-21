d19d3ed5c4e854a7ebfd0d3b8fb8e6c9fd9576f7
Prasun <109636413+Puffy3@users.noreply.github.com>
2023-08-21 23:25:16 +0530

    Add files via upload

---
+++ Ecolution/myscripts.js
@@ -0,0 +1,30 @@
+function myFunction()
+{
+    alert("This service is not yet available!!");
+}
+function service()
+{
+    alert("This service is not yet available!!")
+}
+
+// function show() {
+   
+//     document.getElementsByClassName("image")
+//         .style.display = "block";
+//     document.getElementsByClassName("btnID")
+//         .style.display = "none";
+// }
+// function search_txt() {
+//     let input = document.getElementsByClassName("srchbar").value
+//     input=input.toLowerCase();
+//     let x = document.getElementsByClassName("article");
+      
+//     for (i = 0; i < x.length; i++) { 
+//         if (!x[i].innerHTML.toLowerCase().includes(input)) {
+//             x[i].style.display="none";
+//         }
+//         else {
+//             x[i].style.display="list-item";                 
+//         }
+//     }
+// }
